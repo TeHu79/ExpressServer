56983eb0640ccfdcc00610c839f7ffa41375d2a7
Teija Hurme <teija.hurme@student.careeria.fi>
2025-02-02 15:39:34 +0200

    jatkettiin koodin muokkausta

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //Express.js palvelin//
 const path = require('path')
 const express = require('express')
+const fs = require('fs').promises
 
 const app = express()
 
@@ -10,6 +11,24 @@ const herkut = require('./herkut.json')
 app.get('/api/herkut', (req, res) => {
     res.json(herkut)
     })
+
+
+
+// Pinkoodin lukeminen txt tiedostosta palvelimelta ja lähettäminen selaimelle
+app.get('/api/getpin', async (req, res) => {
+    try {
+        // Read the content of the text file
+        const savedPin = await fs.readFile('./pin.txt', 'utf-8')
+
+        // Send the file content of the response
+        res.send(savedPin)
+    } catch (error) {
+        console.error('Error reading file:', error)
+        res.status(500).send('Internal Server Error')
+    } 
+    })
+
+
 
 //Tehdään polkumääritys public kansioon
 const polku = path.join(__dirname, './public')
